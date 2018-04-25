d6f7225ff36a605bfeb7c80b0d86b33d7416c8bb
Agustin_Macias <xavier809@outlook.es>
2018-04-24 19:16:56 -0500

    corr

---
+++ corr.R
@@ -0,0 +1,30 @@
+directorio = "C:/Users/xavie/OneDrive/Documentos/GitHub/Software_Actuarial_III/specdata"
+corr <- function(directorio,horizonte = 0){
+  id = 1:332
+
+  for (i in id){
+  
+  if (i < 10){
+    lets <- paste("C:/Users/xavie/OneDrive/Documentos/GitHub/Software_Actuarial_III/specdata/00",i,".csv",sep="")
+    unore <- read.csv(lets, header = T)
+    unore
+  }else if (i >= 10 & i < 100 ){
+    lets <- paste("C:/Users/xavie/OneDrive/Documentos/GitHub/Software_Actuarial_III/specdata/0",i,".csv",sep="")
+    unore <- read.csv(lets, header = T)
+    unore
+  }else if (i >= 100){
+    lets <- paste("C:/Users/xavie/OneDrive/Documentos/GitHub/Software_Actuarial_III/specdata/",i,".csv",sep="")
+    unore <- read.csv(lets, header = T)
+    unore
+  }
+  
+  torqo <- na.exclude(unore)
+  torqo
+  soler <- cor(torqo$sulfate,torqo$nitrate)
+  
+  print(soler)
+  }
+  
+}
+
+corr(directorio,horizonte = 0)
