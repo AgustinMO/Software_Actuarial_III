7b377533f03d470fa054bcce8d8d04fdedfb8c2d
Agustin_Macias <xavier809@outlook.es>
2018-04-24 19:16:12 -0500

    mediacontaminante

---
+++ mediacontaminante.R
@@ -0,0 +1,36 @@
+directorio = "C:/Users/xavie/OneDrive/Documentos/GitHub/Software_Actuarial_III/specdata"
+mediacontaminante <- function(directorio,contaminante,id){
+  
+  for (i in id){
+  
+  if (i < 10){
+    lets <- paste("C:/Users/xavie/OneDrive/Documentos/GitHub/Software_Actuarial_III/specdata/00",i,".csv",sep="")
+    unore <- read.csv(lets, header = T)
+    unore
+  }else if (i >= 10 & i < 100 ){
+    lets <- paste("C:/Users/xavie/OneDrive/Documentos/GitHub/Software_Actuarial_III/specdata/0",i,".csv",sep="")
+    unore <- read.csv(lets, header = T)
+    unore
+  }else if (i >= 100){
+    lets <- paste("C:/Users/xavie/OneDrive/Documentos/GitHub/Software_Actuarial_III/specdata/",i,".csv",sep="")
+    unore <- read.csv(lets, header = T)
+    unore
+  }
+  
+  
+  if (contaminante == "sulfate"){
+    pl <- data.frame(unore$sulfate)
+    ipl <- colMeans(pl,na.rm = TRUE)            
+    ipl
+  }else if (contaminante == "nitrate"){
+    sl <- data.frame(unore$nitrate)
+    ipl <- colMeans(sl,na.rm = TRUE)
+    ipl
+  }
+    print(ipl)
+  
+  }
+  
+}
+
+mediacontaminante(directorio,contaminante = "sulfate", id = 1:332)
